ec0ef568294430a8a3d699c0b2b772088d11c0f8
lapulga248 <brianpalma248@gmail.com>
2024-10-13 18:59:09 +0800

    Added the Second API Integration w/ Comments (Version 6)
    Version 6 - API Integration of Humidity & Windspeed Features

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let countrycityInput = document.getElementById('countrycity_input'),
-checkweatherBtn = document.getElementById('checkweatherBtn'),
-api_key = '9672eb7d4fd80e69190c786a1f4332a0',
-currentWeatherCard = document.querySelectorAll('.weatherLeft .cardToday')[0];
+let countrycityInput = document.getElementById('countrycity_input'), // let declares the data inside it as variables. This variable has a method to select an HTML element by its ID.
+checkweatherBtn = document.getElementById('checkweatherBtn'), // This variable has a method which is getElementById to select an HTML element by its ID which is the checkweatherBtn.
+api_key = '9672eb7d4fd80e69190c786a1f4332a0', // This code is the OpenWeatherMap API Key that can access to the OpenWeatherMap service.
+currentWeatherCard = document.querySelectorAll('.weatherLeft .cardToday')[0], // This code has a method which is querySelectorAll to select the first element in a group of elements in CSS.
+humidityValue = document.getElementById('humidityValue'), // This variable has a method which is getElementById to select an HTML element by its ID which is the humidityValue.
+windspeedValue = document.getElementById('windspeedValue'); // This variable has a method which is getElementById to select an HTML element by its ID which is the windspeedValue.
 
-function checkWeatherDetails(name, lat, lon, country, state){
-	let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
-	days = [
+function checkWeatherDetails(name, lat, lon, country, state){ // This code defines the first function called checkWeatherDetails that has five parameters. 
+	let WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`, // This is the URL of OpenWeatherMap API to fetch weather data using lati & longi.
+	days = [ // Two arrays that contains the days of the week & months of the year.
 		'Sunday',
 		'Monday',
 		'Tuesday',
@@ -14,7 +16,7 @@ function checkWeatherDetails(name, lat, lon, country, state){
 		'Friday',
 		'Saturday'
 		],
-	months = [
+	months = [ 
 		'January',
 		'February',
 		'March',
@@ -29,9 +31,9 @@ function checkWeatherDetails(name, lat, lon, country, state){
 		'December'
 		];
 
-	fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-		let date = new Date();
-		currentWeatherCard.innerHTML = `
+	fetch(WEATHER_API_URL).then(res => res.json()).then(data => { // This fetch code sends a request to the OpenWeatherMap API and the response is converted into a JSON Format. 
+		let date = new Date(); // This code creates a date variable and the new Date() code creates a date object to get the current date and time.
+		currentWeatherCard.innerHTML = ` 
 			<div class="weatherCurrent">
 				<div class="todayDetails">
 					<p>Today</p>
@@ -47,23 +49,29 @@ function checkWeatherDetails(name, lat, lon, country, state){
 				<p><i class="fa-light fa-calendar"></i> ${days[date.getDay()]}, ${date.getDate()}, ${months[date.getMonth()]}, ${date.getFullYear()}</p>
 				<p><i class="fa-light fa-location-dot"></i> ${name}, ${country}</p>
 			</div>
-		`;
+		`; // When current weather data is retrieved, the currentWeatherCard updates the contents of the HTML codes inside it.
+		   // The information contains the current temperature, weather description, icons that represents the current weather, current date and time, and the city & country name.
+		   // The ${__} code is used to reference a variable within a string.
+		let {humidity} = data.main, // This code uses the destructuring technique which is to only get the humidity & windspeed specifically from the API Response.
+		{speed} = data.wind; 
+		humidityValue.innerHTML = `${humidity}%`; // This code updates the HTML Contents to show the humidity's percentage & windspeed's meters per second.
+		windspeedValue.innerHTML = `${speed}m/s`; 
 	}).catch(() => {
-		alert('Failed to fetch current weather');
+		alert('Failed to fetch current weather'); // If the fetch request fails, this catch code then displays the alert code message inside it.
 	});
 }
 
-function checkCountryCityCoordinates(){
-	let countrycityName = countrycityInput.value.trim();
-	countrycityInput.value = '';
-	if(!countrycityName) return;
-	let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${countrycityName}&limit=1&appid=${api_key}`;
-	fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
-		let {name, lat, lon, country, state} = data[0];
-		checkWeatherDetails(name, lat, lon, country, state);
+function checkCountryCityCoordinates(){ // This code defines the second function called checkCountryCityCoordinates that executes when the checkweatherBtn is clicked. 
+	let countrycityName = countrycityInput.value.trim(); // This code retrieves the value entered by the user and it trims any whitespace or extra space on it.
+	countrycityInput.value = ''; // This code clears the input field. 
+	if(!countrycityName) return; // This code if the input field is empty, it returns.
+	let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${countrycityName}&limit=1&appid=${api_key}`; // This is the URL of Geocoding API to fetch geographic coordinates of the city.
+	fetch(GEOCODING_API_URL).then(res => res.json()).then(data => { // This fetch code sends a request to the OpenWeatherMap Geocoding API and the response is converted into a JSON Format. 
+		let {name, lat, lon, country, state} = data[0]; 
+		checkWeatherDetails(name, lat, lon, country, state); // When the data is retrieved, This code calls the function checkWeatherDetails with the retrieved data and display it.
 	}).catch(() => {
-		alert('Failed to fetch coordinates of ${countrycityName}');
+		alert('Failed to fetch coordinates of ${countrycityName}'); // If the fetch request fails, this catch code then displays the alert code message inside it.
 	});
 }
 
-checkweatherBtn.addEventListener('click', checkCountryCityCoordinates); 
+checkweatherBtn.addEventListener('click', checkCountryCityCoordinates); // This code has an event listener and when the button is clicked, it calls the function checkCountryCityCoordinates.
